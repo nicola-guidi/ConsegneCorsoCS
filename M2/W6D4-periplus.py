6bc1622311b7956b5003adb7891731ed0e1744ff
Nicola Guidi <159790006+nicola-guidi@users.noreply.github.com>
2025-11-05 11:54:59 +0100

    Update W6D4-periplus.py

--- M2/W6D4-periplus.py
+++ M2/W6D4-periplus.py
@@ -1,5 +1,7 @@
+# IMPORT DELLE LIBRERIE
 import math
 
+# FUNZIONI DEI VARI CALCOLI
 def calcolo_per_quad(lato_quad):
     return lato_quad * 4
 
@@ -18,6 +20,7 @@ def calcolo_perimetro_triangolo(lato_tri):
 def calcolo_area_triangolo(lato_tri):
     return ((lato_tri ** 2) * math.sqrt(3)) / 4
 
+# FUNZIONI RELATIVE ALLA SCELTA DELLE FIGURE
 def scelta_quadrato():
     perimetro_quad_2 = round(calcolo_per_quad(area), 2)
     area_quad_2 = round(calcolo_area_quad(area), 2)
@@ -33,6 +36,7 @@ def scelta_cerchio():
     area_cer_2 = round(calcolo_area_cerchio(area), 2)
     print("Se l'area calcolata prima fosse uguale al raggio del cerchio, la sua circonferenza sarebbe " + str(circonferenza_2) + " e la sua area " + str(area_cer_2))
 
+# INIZIO DEL PROGRAMMA CON SCELTA DELLA FIGURA INIZIALE
 print("Scegli una figura:\n")
 print(" 1. Quadrato")
 print(" 2. Cerchio")
@@ -40,6 +44,7 @@ print(" 3. Triangolo (equilatero)\n")
 
 figura_1 = int(input(">>> "))
 
+# PRIMO BLOCCO IF/ELSE
 if figura_1 == 1:
     lato_quad = int(input("Bene, iniziamo dal quadrato. Inserisci un numero: "))
     perimetro_quad = calcolo_per_quad(lato_quad)
@@ -52,6 +57,8 @@ if figura_1 == 1:
     print(" 2. Triangolo (equilatero)\n")
 
     figura_2 = int(input(">>> "))
+
+    # SECONDO BLOCCO (NETSTED) IF/ELSE
     if figura_2 == 1:
         print("Bene, proseguiamo con il cerchio")   
         scelta_cerchio()
@@ -61,7 +68,7 @@ if figura_1 == 1:
         scelta_triangolo()
         scelta_cerchio()
     else:
-        print("Selezione non valida!")
+        print("Selezione non valida!") # ERROR HANDLING
 
 elif figura_1 == 2:
     raggio = int(input("Bene, iniziamo dal cerchio. Inserisci un numero: "))
@@ -69,12 +76,14 @@ elif figura_1 == 2:
     area_cer = calcolo_area_cerchio(raggio)
     area = area_cer
 
-    print("Se il numero che mi hai dato fosse il raggio di un cerchio, la sua circonferenza sarebbe " + str(round(circonferenza, 2)) + " e la sua area " + str(round(area, 2)))
+    print("Se il numero che mi hai dato fosse il raggio di un cerchio, la sua circonferenza sarebbe " + str(round(circonferenza, 2)) + " e la sua area " + str(round(area, 2))) # FUNZIONE ROUND PER ARROTONDARE A 2 DECIMALI
     print("Scegli una nuova figura:\n")
     print(" 1. Quadrato")
     print(" 2. Triangolo (equilatero)\n")
     
     figura_2 = int(input(">>> "))  
+
+    # TERZO BLOCCO (NETSTED) IF/ELSE
     if figura_2 == 1:   
        print("Bene, proseguiamo con il quadrato")
        scelta_quadrato()
@@ -84,21 +93,22 @@ elif figura_1 == 2:
        scelta_triangolo()
        scelta_quadrato()
     else:
-        print("Selezione non valida!")     
+        print("Selezione non valida!") # ERROR HANDLING     
       
 elif figura_1 == 3:
     lato_tri = int(input("Bene, iniziamo dal triangolo equilatero. Inserisci un numero: "))
     perimetro_tri = calcolo_perimetro_triangolo(lato_tri)
     area_tri = calcolo_area_triangolo(lato_tri)
-    round_area_tri = round(area_tri, 2)
-    area = round_area_tri
+    area = area_tri
 
-    print("Se il numero che mi hai dato fosse il lato di un triangolo equilatero, il suo perimetro sarebbe " + str(perimetro_tri) + " e la sua area " + str(round_area_tri))
+    print("Se il numero che mi hai dato fosse il lato di un triangolo equilatero, il suo perimetro sarebbe " + str(perimetro_tri) + " e la sua area " + str(round(area_tri, 2))) # FUNZIONE ROUND PER ARROTONDARE A 2 DECIMALI
     print("Scegli una nuova figura:\n")
     print(" 1. Quadrato")
     print(" 2. Cerchio\n")
 
-    figura_2 = int(input(">>> "))   
+    figura_2 = int(input(">>> "))  
+
+    # QUARTO BLOCCO (NETSTED) IF/ELSE
     if figura_2 == 1:   
        print("Bene, proseguiamo con il quadrato")
        scelta_quadrato()
@@ -108,6 +118,6 @@ elif figura_1 == 3:
        scelta_cerchio()
        scelta_quadrato()
     else:
-        print("Selezione non valida!")   
+        print("Selezione non valida!") # ERROR HANDLING   
 else:
-    print("Selezione non valida!")
+    print("Selezione non valida!") # ERROR HANDLING
